da9451fddb41500bba85d8073c34d525303c2bc3
ssomakim <sojung9331@gmail.com>
2026-02-17 23:33:19 +0900

    fix: remove tsx extension from Bgm import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Bgm from "./ui/Bgm.tsx";
+import Bgm from "./ui/Bgm";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
